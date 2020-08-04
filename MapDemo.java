636fcc27728b9c1a7f7b0b6e18434326f4e3a102
leah4 <50786472+leah4@users.noreply.github.com>
2020-08-04 11:31:12 +0000

    Update MapDemo.java

--- MapDemo.java
+++ MapDemo.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MapDemo {
+
     public static void main(String[] args) {
         Map<Integer,String> map = new HashMap<>();
         map.put(1, "hello");
